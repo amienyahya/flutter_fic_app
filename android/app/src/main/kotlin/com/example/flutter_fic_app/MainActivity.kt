d45baf89b6171b120d02af79785d685b90a96b32
amienyahya <amien.yahya5@gmail.com>
2024-01-13 23:36:19 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_fic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/global_variables.dart
@@ -0,0 +1,5 @@
+class GlobalVariables {
+  static const baseUrl = 
+  'https://51f2-182-253-197-50.ngrok-free.app';
+
+}
